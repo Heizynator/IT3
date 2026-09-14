f177250a4d60682bf3f429de0b74516fb2674cc3
Heizy <heizyeu@gmail.com>
2026-09-14 12:20:29 +0200

    TŘETÍ KRŮČEK

--- classNEW.py
+++ classNEW.py
@@ -4,18 +4,25 @@ class Zvire:
         self.age = age
         self.misto = misto 
 
+
     def zvuk(self):
         return "???"
 
     def predstavse(self):
         return f"Jsem {self.name} a je mi {self.age}"
 
+    def kdejsi(self):
+        return f"Jsem v místě {self.misto}"
 
-
-
+    def jditam(self, nMisto:str):
+        self.misto = nMisto
+        return f"Přesunul jsem se na {nMisto}. {self.kdejsi()}"
+    
 zvire = Zvire("Ludek", 13)
 zvire1 = Zvire("Pepa", 18, "Domov")
 
 print(zvire1.name)
 print(zvire1.age)
 print(zvire1.predstavse())
+print(zvire1.kdejsi())
+print(zvire1.jditam("Skola"))
